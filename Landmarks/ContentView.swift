d0d65d58419bffcf4d1375cd6d7911be56de2668
Roy Tang <roytang1005@qq.com>
2020-04-15 10:25:50 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by WCJ on 2020/4/15.
+//  Copyright © 2020 WCJ. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
